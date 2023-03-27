f2d917b3b49e607b90e04660b1ab991cf31a5fed
ShoonLei98 <106812215+ShoonLei98@users.noreply.github.com>
2023-03-27 23:03:54 +0630

    custom form api CRUD

--- app/Http/Controllers/CustomFormApiController.php
+++ app/Http/Controllers/CustomFormApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\CustomForm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class CustomFormApiController extends Controller
 {
@@ -12,7 +13,12 @@ class CustomFormApiController extends Controller
      */
     public function index()
     {
-        //
+        $customForm = CustomForm::get();
+        $response = [
+            'status' => "success",
+            'data' => $customForm
+        ];
+        return Response::json($response);
     }
 
     /**
@@ -20,30 +26,88 @@ class CustomFormApiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $customForm = new CustomForm();
+        $customForm->name = $request->name;
+        $customForm->phone = $request->phone;
+        $customForm->birth_date = $request->birth_date;
+        $customForm->gender = $request->gender;
+        $customForm->save();
+        $response = [
+            'status' => 200,
+            'message' => 'success',
+        ];
+
+        return Response::json($response);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(CustomForm $customForm)
+    public function show($id)
     {
-        //
+        $data = CustomForm::find($id);
+        if(!empty($data)){
+            return Response::json([
+                'status' => 200,
+                'message' => 'success',
+                'data' => $data
+            ]);
+        }
+        else{
+            return Response::json([
+                'status' => 200,
+                'message' => 'fail',
+                'data' => $data
+            ]);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, CustomForm $customForm)
+    public function update(Request $request, $id)
     {
-        //
+        $customForm = [
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'birth_date' => $request->birth_date,
+            'gender' => $request->gender,
+        ];
+        
+        $form = CustomForm::find($id);
+        
+        if(!empty($form)){
+            CustomForm::where('id', $id)->update($customForm);
+            return Response::json([
+                'result' => 200,
+                'message' => 'success'
+            ]);
+        }
+        else{
+            return Response::json([
+                'result' => 200,
+                'message' => 'There is no data to update.'
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(CustomForm $customForm)
+    public function destroy($id)
     {
-        //
+        $form = CustomForm::find($id);
+        
+        if(empty($form)){
+            return Response::json([
+                'result' => 200,
+                'message' => 'There is no data'
+            ]);
+        }
+        CustomForm::where('id', $id)->delete($form);
+        return Response::json([
+            'result' => 200,
+            'message' => 'success'
+        ]);
     }
 }
